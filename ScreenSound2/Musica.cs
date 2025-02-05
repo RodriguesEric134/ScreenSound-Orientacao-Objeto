6ce986c90d674bf2f1b7e226d1d007422198acc8
RodriguesEric134 <rm550249@fiap.com.br>
2025-02-05 14:14:40 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound2/Musica.cs
@@ -0,0 +1,28 @@
+﻿/* o que tem em uma música?
+ * artista - string
+ * nome - string
+ * duração -int
+ * disponibilidade - booleano (true or false)
+ */
+class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine("disponível no plano");
+        }
+        else
+        {
+            Console.WriteLine("Música indisponível no plano, adiquira o plano mega plus ultra mais");
+        }
+    }
+}
