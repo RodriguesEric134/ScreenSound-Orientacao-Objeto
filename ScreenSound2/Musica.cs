d7f6855508b587f3ac4b3d4400ae4674bc29be0f
RodriguesEric134 <rm550249@fiap.com.br>
2025-02-05 15:55:55 -0300

    firme, forte e de cabeça quente

--- ScreenSound2/Musica.cs
+++ ScreenSound2/Musica.cs
@@ -1,28 +1,30 @@
-﻿/* o que tem em uma música?
- * artista - string
- * nome - string
- * duração -int
- * disponibilidade - booleano (true or false)
+﻿/* O que tem em uma música?
+ * Nome - string
+ * Artista - string
+ * Duração - int
+ * Disponibilidade - bool (true ou false)
+ * DescriçãoResumida - string (gerada a partir do Nome e Artista, e somente de leitura)
  */
 class Musica
 {
-    public string nome;
-    public string artista;
-    public int duracao;
-    public bool disponivel;
+    public string Nome { get; set; } // Nome da música
+    public string Artista { get; set; } // Nome do artista
+    public int Duracao { get; set; } // Duração da música em segundos (ou minutos, conforme o caso)
+    public bool Disponivel { get; set; } // Indica se a música está disponível (true) ou não (false)
+    public string DescricaoResumida => $"A música {Nome} pertence à banda {Artista}"; // Método lambda que gera uma descrição resumida da música e é somente leitura
 
-    public void ExibirFichaTecnica()
+    public void ExibirFichaTecnica() // Método que exibe informações sobre a música
     {
-        Console.WriteLine($"Nome: {nome}");
-        Console.WriteLine($"Artista: {artista}");
-        Console.WriteLine($"Duração: {duracao}");
-        if (disponivel)
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração: {Duracao}");
+        if (Disponivel)
         {
-            Console.WriteLine("disponível no plano");
+            Console.WriteLine("Disponível no plano");
         }
         else
         {
-            Console.WriteLine("Música indisponível no plano, adiquira o plano mega plus ultra mais");
+            Console.WriteLine("Música indisponível no plano, adquira o plano mega plus ultra mais");
         }
     }
 }
